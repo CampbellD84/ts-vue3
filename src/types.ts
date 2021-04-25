9f620c1f62d0214ea058afd9cea0720a316b14da
Douglas Campbell <dacampbelltbone@gmail.com>
2021-04-24 23:49:00 -0400

    Created Todo and set it up to use the new Composition API.

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,19 @@
 // File stores types to use for this app
-
 export interface EventItem {
-  "id": number,
-  "category": string,
-  "title": string,
-  "description": string,
-  "location": string,
-  "date": string,
-  "time": string,
-  "organizer": string
+  id: number,
+  category: string,
+  title: string,
+  description: string,
+  location: string,
+  date: string,
+  time: string,
+  organizer: string
+}
+
+type TaskType = 'personal' | 'work' | 'miscellaneous'
+
+export interface TodoItem {
+  label: string
+  type: TaskType
+  isComplete: boolean
 }
